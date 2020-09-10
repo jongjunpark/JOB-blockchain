63bb6b8cf17dcf646fbcc4976684c83d06484e6f
kyubot <tera0724@naver.com>
2020-09-10 17:00:40 +0900

    회원가입시 이메일 인증 완료 .S03P22B104-70

--- backend-django/backend/accounts/urls.py
+++ backend-django/backend/accounts/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, include
 from . import views
 
+
 app_name = 'accounts'
 
 urlpatterns = [
   path('', views.user),
+  path('send/<str:email>', views.email),
 ]
+
--- backend-django/backend/backend/settings.py
+++ backend-django/backend/backend/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # test
+    'django_extensions',
+
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -155,3 +159,9 @@ REST_FRAMEWORK = {
 CORS_ORIGIN_ALLOW_ALL = True
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = True

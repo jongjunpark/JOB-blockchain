a4b2f8a8c8b830aae83c838372aed91239a0c250
박종준 <poiufgin7373@naver.com>
2020-09-24 17:39:06 +0900

    add video skeleton page .S03P23B104-48

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
 import Resume from '../views/Resume.vue'
+import Video from '../views/Video.vue'
 
 Vue.use(VueRouter)
 
@@ -21,6 +22,11 @@ Vue.use(VueRouter)
     path: '/resume',
     name: 'Resume',
     component: Resume
+  },
+  {
+    path: '/video',
+    name: 'Video',
+    component: Video
   }
 ]
 
